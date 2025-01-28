a5f2b9c389df7227591b6fdaff6e2868fc967d5c
Dag_Hjermann <dhj@niva.no>
2025-01-28 21:22:42 +0100

    app1: data and plot 1 fixed

--- proref_app1/app.R
+++ proref_app1/app.R
@@ -12,15 +12,82 @@ source("app_functions.R")
 #
 
 datasets <- tibble::tribble(
-  ~analysis_name, ~fn_rawdata, ~fn_series, ~fn_result,
-  "Original (1992-2022)", "54_data_2024.rds", "54_dataseries_2024.rds", "54_result_detailed_2024-11-29.rds",
-  "Original (2003-2022)", "54_data_2024.rds", "54_dataseries_2024.rds", "54_result_detailed_2003-2022_2025-01-05-T1812.rds",
-  "LOQ-filtered (2003-2022)", "54_data_2024.rds", "54_dataseries_2024.rds", "54_result_detailed_2003-2022_2025-01-20-T1603.rds"
+  ~analysis_name, ~fn_rawdata, ~fn_series, ~fn_result, ~year1, ~year2,
+  "Original (1992-2022)", "54_data_2024.rds", "54_dataseries_2024.rds", "54_result_detailed_2024-11-29.rds", 1992, 2022,
+  "Original (2003-2022)", "54_data_2024.rds", "54_dataseries_2024.rds", "54_result_detailed_2003-2022_2025-01-05-T1812.rds", 2003, 2022,
+  "LOQ-filtered (2003-2022)", "54_data_2024_loqfilter3x.rds", "54_dataseries_2024_loqfilter3x.rds", "54_result_detailed_2003-2022_2025-01-20-T1603.rds", 2003, 2022
 )
+
+
+read_data <- function(fn){
+  readRDS(paste0("../Data/", fn))
+}
+
+result_detailed <- bind_rows(
+  bind_cols(data.frame(Analysis = datasets$analysis_name[1]), read_data(datasets$fn_result[1])),
+  bind_cols(data.frame(Analysis = datasets$analysis_name[2]), read_data(datasets$fn_result[2])),
+  bind_cols(data.frame(Analysis = datasets$analysis_name[3]), read_data(datasets$fn_result[3]))
+) %>%
+  mutate(Analysis = fct_inorder(Analysis))
+
+
+#
+# Calculate proref value ----
+#
+
+lookup_background <- result_detailed %>%
+  select(Analysis, Station, LATIN_NAME, PARAM, Background1b) %>%
+  rename(Background = Background1b)
+
+#
+# .-- raw data ----
+# 
+data_all_comb <- bind_rows(
+  bind_cols(data.frame(Analysis = datasets$analysis_name[1]), read_data(datasets$fn_rawdata[1]) %>% filter(YEAR %in% datasets$year1[1]:datasets$year2[1])),
+  bind_cols(data.frame(Analysis = datasets$analysis_name[2]), read_data(datasets$fn_rawdata[2]) %>% filter(YEAR %in% datasets$year1[2]:datasets$year2[2])),
+  bind_cols(data.frame(Analysis = datasets$analysis_name[3]), read_data(datasets$fn_rawdata[3]) %>% filter(YEAR %in% datasets$year1[3]:datasets$year2[3]))
+) %>%
+  mutate(Analysis = fct_inorder(Analysis))
+
+#
+# .-- background stations ----
+#
+data_backgr_all <- lookup_background %>%
+  # These parameters are only found in raw data file number 1 - they were fixed later
+  filter(!grepl("SCCP__", PARAM)) %>%
+  filter(!grepl("MCCP__", PARAM)) %>%
+  filter(!grepl("Krysen__", PARAM)) %>%
+  select(Analysis, Station, LATIN_NAME, PARAM, Background) %>%
+  left_join(data_all_comb %>%
+              filter(!is.na(Concentration)) %>%
+              select(Analysis, Station = STATION_CODE, LATIN_NAME, PARAM, YEAR, Concentration, FLAG1), 
+            by = join_by(Analysis, Station, LATIN_NAME, PARAM), 
+            relationship = "one-to-many") %>%
+  filter(!is.na(Background)) %>%
+  mutate(
+    Station_bg = case_when(
+      Background %in% "Other" ~ NA,
+      Background %in% "Background" ~ Station)
+  ) %>%
+  arrange(LATIN_NAME, PARAM, Analysis, desc(Background), Station_bg) %>%
+  mutate(LOQ = case_when(
+    is.na(FLAG1) ~ "Over LOQ",
+    TRUE ~ "Under LOQ"))
+
+#
+# .-- proref value (90th percentile)----
+#
+data_proref <- data_backgr_all %>% 
+  filter(Background %in% "Background") %>%
+  group_by(Analysis, LATIN_NAME, PARAM) %>%
+  summarize(PROREF = quantile(Concentration, 0.9) %>% signif(3),
+            Median = quantile(Concentration, 0.5) %>% signif(3),
+            .groups = "drop")
 
 params_menu <- readRDS(paste0("../Data/", datasets$fn_result[3])) %>%
   pull(PARAM) %>%
   unique()
+
 
 #
 # UI ----------------------------------------------------------------------------
@@ -41,16 +108,16 @@ ui <- fluidPage(
                   selected = datasets$analysis_name[2]),
       
       # Parameter selection with text input for partial matching
-      # textInput("param", "Parameter (partial matching)", value = "CU__WW"),
+      # textInput("param", "Parameter (partial matching)", value = "CB118__WW"),
       selectizeInput("param", "Parameter", 
                      choices = params_menu, 
-                     selected = "CD__WW", multiple = FALSE,
+                     selected = "CB118__WW", multiple = FALSE,
                      options = NULL),
       
       # Species selection
       selectInput("species", "Species",
                   choices = c("mussel", "cod"),
-                  selected = "mussel"),
+                  selected = "cod"),
       
       # Add info about the selected parameter
       verbatimTextOutput("param_info")
@@ -80,87 +147,48 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   # reactive: selected_analysis ----
-  selected_analysis <- reactive({
-    
-    
-    d1 <- which(datasets$analysis_name == input$analysis1)
-    d2 <- which(datasets$analysis_name == input$analysis2)
-    result_txt1 <- datasets$analysis_name[d1]
-    result_txt2 <- datasets$analysis_name[d2]
-    
-    # NOTE: these two uses only analysis 1 data:  
-    data_all2 <- readRDS(paste0("../Data/", datasets$fn_rawdata[d1]))
-    df_series_sel <- readRDS(paste0("../Data/", datasets$fn_series[d1]))
-    
-    result_detailed1 <- readRDS(paste0("../Data/", datasets$fn_result[d1]))
-    result_detailed2 <- readRDS(paste0("../Data/", datasets$fn_result[d2]))
-    
-    result_detailed <- bind_rows(
-      bind_cols(data.frame(Analysis = result_txt1), result_detailed1),
-      bind_cols(data.frame(Analysis = result_txt2), result_detailed2)
-    ) %>%
-      mutate(Analysis = fct_inorder(Analysis))
-    
-    #
-    # Raw data with 'Background' classification   
-    #
-    lookup_background <- result_detailed %>%
-      select(Analysis, Station, LATIN_NAME, PARAM, Background1b) %>%
-      rename(Background = Background1b)
-    
-    data_all_backgr <- data_all2 %>%
-      ungroup() %>%
-      select(Station = STATION_CODE, LATIN_NAME, PARAM, YEAR, Concentration, FLAG1) %>%
-      # this is a many-to-many relation since 'lookup_background' have 2 "Analysis" versions for each parameter/station:
-      left_join(lookup_background, relationship = "many-to-many") %>%
-      filter(!is.na(Background)) %>%
-      mutate(
-        Station_bg = case_when(
-          Background %in% "Other" ~ NA,
-          Background %in% "Background" ~ Station)
-      ) %>%
-      arrange(LATIN_NAME, PARAM, Analysis, desc(Background), Station_bg) %>%
-      mutate(LOQ = case_when(
-        is.na(FLAG1) ~ "Over LOQ",
-        TRUE ~ "Under LOQ"))
-    
-    # if uncommented:
-    # browser()
-    # ...this shows that
-    # lookup_background has 29375 rows
-    # data_all2 has 2223050 rows
-    # data_all_backgr has 3570728 rows
-    
-    
-    #
-    # Proref
-    #
-    data_proref <- data_all_backgr %>% 
-      filter(Background %in% "Background") %>%
-      group_by(Analysis, LATIN_NAME, PARAM) %>%
-      summarize(PROREF = quantile(Concentration, 0.9) %>% signif(3))
-    
-    list(
-      result_txt1 = result_txt1,
-      result_txt2 = result_txt2,
-      data_all2 = data_all2,
-      df_series_sel = df_series_sel,
-      result_detailed = result_detailed,
-      data_all_backgr = data_all_backgr,
-      data_proref = data_proref
-    )
-    
-  })
+  # selected_analysis <- reactive({
+  #   
+  #   d1 <- which(datasets$analysis_name == input$analysis1)
+  #   d2 <- which(datasets$analysis_name == input$analysis2)
+  #   result_txt1 <- datasets$analysis_name[d1]
+  #   result_txt2 <- datasets$analysis_name[d2]
+  #   
+  # 
+  #   
+  #   # if uncommented:
+  #   # browser()
+  #   # ...this shows that
+  #   # lookup_background has 29375 rows
+  #   # data_all2 has 2223050 rows
+  #   # data_all_backgr has 3570728 rows
+  #   
+  #   
+  #   #
+  #   # Proref
+  #   #
+  #   data_proref <- data_all_backgr %>% 
+  #     filter(Background %in% "Background") %>%
+  #     group_by(Analysis, LATIN_NAME, PARAM) %>%
+  #     summarize(PROREF = quantile(Concentration, 0.9) %>% signif(3))
+  #   
+  #   list(
+  #     result_sel = result_sel,
+  #     data_proref_sel = data_proref_sel,
+  #     data_sel = data_sel,
+  #     data_backgr_sel = data_backgr_sel
+  #   )
+  #   
+  # })
   
   # reactive: selected_data ----
   # Reactive values for filtered data
   selected_data <- reactive({
     
-    req(selected_analysis())
-    data_all2 <- selected_analysis()$data_all2
-    result_detailed <- selected_analysis()$result_detailed
-    data_all_backgr <- selected_analysis()$data_all_backgr
-    data_proref <- selected_analysis()$data_proref
+    analysis1 <- input$analysis1
+    analysis2 <- input$analysis2
+    
+    param <- input$param
     
     # Convert species common name to Latin name
     species <- if (input$species == "mussel") {
@@ -169,44 +197,67 @@ server <- function(input, output, session) {
       "Gadus morhua"
     }
     
+    # . - select data ---- 
+    
+    result_sel <- result_detailed %>%
+      filter(Analysis %in% c(analysis1, analysis2),
+             PARAM == param,
+             LATIN_NAME == species) %>%
+      mutate(Analysis = fct_drop(Analysis))
+    
+    data_proref_sel <- data_proref %>%
+      filter(Analysis %in% c(analysis1, analysis2),
+             PARAM == param,
+             LATIN_NAME == species) %>%
+      mutate(Analysis = fct_drop(Analysis))
+    
+    data_sel <- data_all_comb %>%
+      filter(Analysis %in% c(analysis1, analysis2),
+             PARAM == param,
+             LATIN_NAME == species) %>%
+      mutate(Analysis = fct_drop(Analysis))
+    
+    data_backgr_sel <- data_backgr_all %>%
+      filter(Analysis %in% c(analysis1, analysis2),
+             PARAM == param,
+             LATIN_NAME == species) %>%
+      mutate(Analysis = fct_drop(Analysis))
+    
+    # browser()
+    
+    # req(selected_analysis())
+    # result_sel <- selected_analysis()$result_sel
+    # data_proref_sel <- selected_analysis()$data_proref_sel
+    # data_sel <- selected_analysis()$data_sel
+    # data_backgr_sel <- selected_analysis()$data_backgr_sel
+    
+
     # Filter result_detailed based on partial parameter match
     # param_pattern <- input$param
     # matching_params <- unique(result_detailed$PARAM[grepl(param_pattern, result_detailed$PARAM, ignore.case = TRUE)])
 
-    # Update result_sel
-    result_sel <- result_detailed %>%
-      filter(PARAM %in% input$param & LATIN_NAME == species)
+    # # Update result_sel
+    # result_sel <- result_detailed %>%
+    #   filter(PARAM %in% input$param & LATIN_NAME == species)
     
     # For plotting line at 2x the cleanest station
     line_2x_cleanest <- result_sel %>%
       group_by(Analysis) %>%
       summarize(Median2 = 2*first(Median2))
     
-    # Update data_sel
-    data_sel <- data_all2 %>%
-      filter(PARAM %in% input$param & LATIN_NAME == species)
-    
-    # update data_all_backgr_sel
-    data_all_backgr_sel <- data_all_backgr %>%
-      filter(PARAM %in% input$param & LATIN_NAME %in% species)
-    
-    # calculate proref
-    data_proref_sel <- data_proref %>%
-      filter(PARAM %in% input$param & LATIN_NAME %in% species)
-    
     # For setting default y axis range
-    max_bg = data_all_backgr_sel %>%
+    max_bg = data_backgr_sel %>%
       filter(Background %in% "Background") %>%
       pull(Concentration) %>%
       max()
     
     # For setting colours
-    number_bg_stations <- table(data_all_backgr_sel$Station_bg) %>% length()
+    number_bg_stations <- unique(data_backgr_sel$Station_bg) %>% length()
 
     list(result_sel = result_sel, 
          line_2x_cleanest = line_2x_cleanest,
          data_sel = data_sel,
-         data_all_backgr_sel = data_all_backgr_sel,
+         data_backgr_sel = data_backgr_sel,
          data_proref_sel = data_proref_sel,
          max_bg = max_bg)
     
@@ -224,37 +275,41 @@ server <- function(input, output, session) {
   
   # Plot 1: Algorithm plot ----
   output$plot1 <- renderPlot({
+    
     req(selected_data())
+
     result_sel <- selected_data()$result_sel %>%
       filter(Rank <= input$max_rank)
-    line_2x_cleanest <- selected_data()$line_2x_cleanest
-    data_proref <- selected_data()$data_proref
     
-    if (nrow(result_sel) == 0) return(NULL)
+    # line_2x_cleanest <- selected_data()$line_2x_cleanest
     
-    ggplot(result_sel, aes(Rank, Median2)) +
+    gg <- ggplot(result_sel, aes(Rank, Median2)) +
       geom_line() +
       geom_point(aes(col = Background1b)) +
-      geom_text_repel(aes(label = glue("{Station} (P = {P_round})")), 
-                      hjust = 0, nudge_x = 0.5, 
-                      point.padding = 0.2, box.padding = 0.25, 
-                      direction = "y") +
-      geom_hline(data = line_2x_cleanest, aes(yintercept = Median2), 
-                 linetype = "dashed", colour = "blue3") +
+      geom_text_repel(aes(label = glue("{Station} (P = {P_round})")), hjust = 0, 
+                      nudge_x = 0.5, point.padding = 0.2, box.padding = 0.25, direction = "y") +
+      # Line at 2x the cleanest station
+      geom_hline(data = selected_data()$line_2x_cleanest, aes(yintercept = Median2),
+                linetype = "dashed", colour = "red") +
+      # Proref line
       geom_hline(
-        data = data_proref, aes(yintercept = PROREF), 
-        colour = "red", linetype = "dashed") +
-      geom_text(
-        data = data_proref, aes(x = -Inf, y = Inf, label = paste("PROREF =", PROREF)), 
-        colour = "red", hjust = -0.1, vjust = 1.5) +
+        data = selected_data()$data_proref_sel, aes(yintercept = PROREF),
+        colour = "blue2", linetype = "dashed") +
       expand_limits(y = 0) +
       facet_wrap(vars(Analysis)) +
-      labs(subtitle = "Red line = PROREF, blue line = 2x cleanest station") +
+      labs(subtitle = "Blue line = PROREF, red line = 2x cleanest station") +
       theme_bw() +
       theme(
         strip.text = element_text(size = 12), 
         legend.text =  element_text(size = 12)
-      )
+      ) 
+    
+    if (nrow(result_sel) == 0) { 
+      return(NULL)
+    } else {
+      return(gg)
+    }
+    
   })
   
   # Plot 2: Time range plot ----
